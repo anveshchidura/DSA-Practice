383f31de9acfee3542f2ffefa3803dad31e22366
Anvesh chidura <31155837+anveshchidura@users.noreply.github.com>
2024-02-23 21:54:01 -0500

    Time: 106 ms (42.12%) | Memory: 77.9 MB (5.56%) - LeetSync

--- tree/main/Arrays/217-contains-duplicate/contains-duplicate.cpp
+++ tree/main/Arrays/217-contains-duplicate/contains-duplicate.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int,int> mpp;
-        for(int i = 0;i< nums.size();i++){
-            mpp[nums[i]] = 0;
-        }
-        for(int i = 0;i< nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(int i = 0;i< nums.size();i++){
-            if(mpp[nums[i]] > 1) return true;
-            
-        }
-        return false;
-        
-        
-        
+bool containsDuplicate(vector<int> nums) {
+ 
+    unordered_set<int> set;
+    
+    for(int i = 0; i<nums.size();i++){
+        set.insert(nums[i]);
     }
+    
+    // if it contains duplicate then the size of 
+    // set will not equal to the length of the array
+    if(set.size() < nums.size())
+        return true;
+    
+    return false;
+}
 };
