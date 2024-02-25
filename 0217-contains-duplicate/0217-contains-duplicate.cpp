c131439fccbd7d6c658445ad25c3f5f9378ba5b3
Anvesh chidura <31155837+anveshchidura@users.noreply.github.com>
2024-02-25 15:36:51 -0500

    Time: 131 ms (23.47%), Space: 77.8 MB (5.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+bool containsDuplicate(vector<int> nums) {
+ 
+    unordered_set<int> set;
+    
+    for(int i = 0; i<nums.size();i++){
+        set.insert(nums[i]);
+    }
+    
+    // if it contains duplicate then the size of 
+    // set will not equal to the length of the array
+    if(set.size() < nums.size())
+        return true;
+    
+    return false ;
+}
+};
