4064f55883f8623ed73802e7de2d8f123ba4c683
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-05-07 15:50:40 -0400

    Time: 892 ms (63.84%), Space: 0B (100.00%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+select product_id,new_price as price from
+(
+select *,Row_number() over (partition by product_id order by change_date desc) as rk
+from products
+where change_date <= '2019-08-16'
+) A
+where rk = 1
+union
+select product_id,10 as price
+from products
+group by 1,2
+having min(change_date) > '2019-08-16';
