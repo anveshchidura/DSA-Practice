57d74fff8f358089d683b3fe2c5ce0a2b8bd7d2b
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-04 19:51:30 -0400

    Time: 0 ms (100.00%), Space: 7.1 MB (86.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,20 +3,19 @@ public:
 
     
 int climbStairs(int n) {
-    if (n == 1) {
+    
+    if (n <= 1) {
         return 1;
     }
-    if (n == 2) {
-        return 2;
-    }
+
     int prev1 = 1;
     int prev2 = 1;
     int curr = NULL;
+    
     for(int i =2; i<=n;i++){
         curr = prev1 + prev2;
-        int temp = prev1;
-        prev1 = curr ;
-        prev2 = temp;
+        prev2 = prev1;
+        prev1 = curr ; 
     }
     
     return curr;
