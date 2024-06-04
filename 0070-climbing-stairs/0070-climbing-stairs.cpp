860175ca54ddd266ba034cb316ce2e47608485ce
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-04 18:50:59 -0400

    Time: 2 ms (48.57%), Space: 7.4 MB (31.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    
-int reach(int n, vector<int> &dp){
-    if(n == 0) return 1;
-    if(n < 0 ) return 0;
-    if(dp[n] != 0) return dp[n]; 
-
-    int step1 = reach(n-1, dp);
-    int step2 = reach(n-2, dp);
-
-    return dp[n] = step1 + step2;
-}
 
 int climbStairs(int n) {
-    vector<int> dp(n+1, 0); // Initialize dp with size n+1 and fill with 0
-    return reach(n, dp);
+    vector<int> dp(n+1, 0); 
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i =2; i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+    return dp[n];
 }
 };
