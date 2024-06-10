3953b92b6ebe487c3453866a02a6086fe5b719be
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-10 13:29:36 -0400

    Time: 20 ms (84.34%), Space: 16 MB (82.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,49 +1,30 @@
+/*
+    Given array of coins & an amount, return fewest coins to make that amount
+    Ex. coins = [1,2,5], amount = 11 -> 3, $11 = $5 + $5 + $1
+
+    Compute all min counts for amounts up to i, "simulate" use of a coin
+
+    Time: O(m x n) -> m = # of coins, n = amount
+    Space: O(n)
+*/
+
 class Solution {
 public:
-       
     int coinChange(vector<int>& coins, int amount) {
-    // Create two vectors to store the previous and current DP states
-    int n = coins.size();
-    int T = amount;
-    vector<int> arr = coins;
-    
-    vector<int> prev(T + 1, 0);
-    vector<int> cur(T + 1, 0);
-    
-    // Initialize the first row of the DP table
-    for (int i = 0; i <= T; i++) {
-        if (i % arr[0] == 0)
-            prev[i] = i / arr[0];
-        else
-            prev[i] = 1e9; // Set it to a very large value if not possible
-    }
-    
-    // Fill the DP table using a bottom-up approach
-    for (int ind = 1; ind < n; ind++) {
-        for (int target = 0; target <= T; target++) {
-            // Calculate the minimum elements needed without taking the current element
-            int notTake = prev[target];
-            
-            // Calculate the minimum elements needed by taking the current element
-            int take = 1e9; // Initialize 'take' to a very large value
-            if (arr[ind] <= target)
-                take = 1 + cur[target - arr[ind]];
-                
-            // Store the minimum of 'notTake' and 'take' in the current DP state
-            cur[target] = min(notTake, take);
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        
+        for (int i = 1; i < amount + 1; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
         }
-        // Update the previous DP state with the current state for the next iteration
-        prev = cur;
+        
+        if (dp[amount] == amount + 1) {
+            return -1;
+        }
+        return dp[amount];
     }
-    
-    // The answer is in the last row of the DP table
-    int ans = prev[T];
-    
-    // If 'ans' is still very large, it means it's not possible to form the target sum
-    if (ans >= 1e9)
-        return -1;
-    
-    return ans; // Return the minimum number of elements needed
-}
-    
 };
