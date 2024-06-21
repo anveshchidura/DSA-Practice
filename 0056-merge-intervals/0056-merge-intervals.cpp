0c33260e4bf2a227144f4fa7dfcf97a81dfdb113
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-21 19:02:47 -0400

    Time: 21 ms (74.25%), Space: 22.7 MB (68.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> results;
         sort(intervals.begin(), intervals.end());
         int n = intervals.size();
-        results.push_back({intervals[0][0],intervals[0][1]});
+        results.push_back({intervals[0]});
         for(int i = 1; i < n;i++){
             if(intervals[i][0] <= results.back()[1]){
                 results.back()[1] = max(intervals[i][1],results.back()[1]);
