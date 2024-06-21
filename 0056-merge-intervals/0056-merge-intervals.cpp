7cbfc47a75ff475a551a6dfc3837203155e8d67c
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-21 18:10:35 -0400

    Time: 26 ms (41.20%), Space: 22.7 MB (41.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,14 +3,9 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        if (n == 1) {
-            return intervals;
-        }
-        
+
         sort(intervals.begin(), intervals.end());
-        
         vector<vector<int>> result;
-        
         int i = 0;
         while (i < n - 1) {
             if (intervals[i][1] >= intervals[i+1][0]) {
@@ -22,7 +17,6 @@ public:
             i++;
         }
         result.push_back(intervals[i]);
-        
         return result;
     }
 };
