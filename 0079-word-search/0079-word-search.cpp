f14770427cd48cfc2583a90bd40d98771605ee14
Anvesh Chidura <31155837+anveshchidura@users.noreply.github.com>
2024-06-24 21:27:24 -0400

    Time: 1001 ms (26.58%), Space: 11.7 MB (44.71%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,13 @@
+/*
+    Given a char board & a word, return true if word exists in the grid
+
+    DFS traversal, set visited cells to '#', search in 4 directions, backtrack
+
+    Time: O(n x 3^l) -> n = # of cells, l = length of word
+    Space: O(l)
+*/
+
 class Solution {
-private:
-    bool dfs(int r, int c, int ind, vector<vector<char>>& board, string& word) {
-        if (ind == word.length()) return true;
-        
-        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != word[ind]) {
-            return false;
-        }
-        
-        char original = board[r][c];
-        board[r][c] = '#';  // Mark as visited
-        
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for (auto& dir : directions) {
-            int nrow = r + dir.first;
-            int ncol = c + dir.second;
-            if (dfs(nrow, ncol, ind + 1, board, word)) {
-                board[r][c] = original;  // Restore the original character
-                return true;
-            }
-        }
-        
-        board[r][c] = original;  // Restore the original character
-        return false;
-    }
-    
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
@@ -31,11 +15,37 @@ public:
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0] && dfs(i, j, 0, board, word)) {
-                    return true;
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, 0, i, j, m, n)) {
+                        return true;
+                    }
                 }
             }
         }
+        
+        return false;
+    }
+private:
+    bool dfs(vector<vector<char>>& board, string word,
+        int index, int i, int j, int m, int n) {
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[index]) {
+            return false;
+        }
+        if (index == word.size() - 1) {
+            return true;
+        }
+        
+        board[i][j] = '#';
+        
+        if (dfs(board, word, index + 1, i - 1, j, m, n)
+            || dfs(board, word, index + 1, i + 1, j, m, n)
+            || dfs(board, word, index + 1, i, j - 1, m, n)
+            || dfs(board, word, index + 1, i, j + 1, m, n)) {
+            return true;
+        }
+        
+        board[i][j] = word[index];
         return false;
     }
 };
